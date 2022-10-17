2be8650c51f0f21675472ce4259bf579656b1d8e
nieo2002 <115522184+nieo2002@users.noreply.github.com>
2022-10-17 16:31:26 +0800

    Update poly_generator.py

--- src/poly_generator.py
+++ src/poly_generator.py
@@ -47,90 +47,89 @@ def listDir(rootDir,x_split_size,y_split_size,points_split):
                 polygon = gdf.geometry.to_json()
                 polygon_dict = json.loads(polygon)
                 for i in range(len(polygon_dict["features"])):
-                    points = polygon_dict["features"][i]["geometry"]["coordinates"][0]
+                    for points in polygon_dict["features"][i]["geometry"]["coordinates"]:
+                        new_points = []
+                        polygon_points = points
+                        if len(points) == 1 :
+                            polygon_points = points[0]
+                        for pt in polygon_points:
+                            #x = round(pt[0],4)
+                            x=pt[0] 
+                            y=pt[1] 
+                            #y = round(pt[1],3)
+                            new_points.append((x,y))
+                    
+                        json_points = json.dumps(new_points)    
+                        #print("len: ",len(new_points))
 
-                    new_points = []
-                    for pt in points:
-                        #x = round(pt[0],4)
-                        x=pt[0] 
-                        y=pt[1] 
-                        #y = round(pt[1],3)
-                        new_points.append((x,y))
-                
-                    json_points = json.dumps(new_points)    
-                    #print("len: ",len(new_points))
+                        #X1 = round(gdf.iloc[0].geometry.bounds[0],4)  # 最左
+                        #Y1 = round(gdf.iloc[0].geometry.bounds[1],4)  # 最下
+                        #X2 = round(gdf.iloc[0].geometry.bounds[2],4)  # 最右
+                        #Y2 = round(gdf.iloc[0].geometry.bounds[3],4)  # 最上
+                        X1=gdf.iloc[0].geometry.bounds[0] 
+                        Y1=gdf.iloc[0].geometry.bounds[1] 
+                        X2=gdf.iloc[0].geometry.bounds[2] 
+                        Y2=gdf.iloc[0].geometry.bounds[3] 
 
-                    #X1 = round(gdf.iloc[0].geometry.bounds[0],4)  # 最左
-                    #Y1 = round(gdf.iloc[0].geometry.bounds[1],4)  # 最下
-                    #X2 = round(gdf.iloc[0].geometry.bounds[2],4)  # 最右
-                    #Y2 = round(gdf.iloc[0].geometry.bounds[3],4)  # 最上
-                    X1=gdf.iloc[0].geometry.bounds[0] 
-                    Y1=gdf.iloc[0].geometry.bounds[1] 
-                    X2=gdf.iloc[0].geometry.bounds[2] 
-                    Y2=gdf.iloc[0].geometry.bounds[3] 
+                        x1= X2 - X1
+                        y1 = Y2 - Y1
+                        split_cnt_x = 1
+                        split_cnt_y = 1
+                        flag = False
 
-                    if Y1 < 0 or Y2 < 0: 
-                        xxx=1
+                        if len(new_points) > points_split: 
+                            flag = True
+                            while 1:
+                                if x1 > x_split_size:
+                                    split_cnt_x += 1
+                                    x1 = round((X2 - X1)/split_cnt_x)
+                                    continue
+                                break
+                            while 1:
+                                if y1 > y_split_size:
+                                    split_cnt_y += 1
+                                    y1 = round((Y2 - Y1)/split_cnt_y)
+                                    continue
+                                break
+                        else:
+                            #write csv
+                            data_list.append((X1,Y1,X2,Y2,json_points,city[:city.index('.')]))
+                            continue
 
-                    x1= X2 - X1
-                    y1 = Y2 - Y1
-                    split_cnt_x = 1
-                    split_cnt_y = 1
-                    flag = False
+                        w= (gdf.iloc[0].geometry.bounds[2] - gdf.iloc[0].geometry.bounds[0])/split_cnt_x
+                        h= (gdf.iloc[0].geometry.bounds[3] - gdf.iloc[0].geometry.bounds[1])/split_cnt_y
+                        rect=make_mesh([gdf.iloc[0].geometry.bounds[0],gdf.iloc[0].geometry.bounds[1],gdf.iloc[0].geometry.bounds[2],gdf.iloc[0].geometry.bounds[3]],w,h)
+                        rectangles=gpd.GeoDataFrame(geometry=rect)
+                        rectangles['RectangleID']=range(len(rectangles))
 
-                    if len(new_points) > points_split: 
-                        flag = True
-                        while 1:
-                            if x1 > x_split_size:
-                                split_cnt_x += 1
-                                x1 = round((X2 - X1)/split_cnt_x)
-                                continue
-                            break
-                        while 1:
-                            if y1 > y_split_size:
-                                split_cnt_y += 1
-                                y1 = round((Y2 - Y1)/split_cnt_y)
-                                continue
-                            break
-                    else:
-                        #write csv
-                        data_list.append((X1,Y1,X2,Y2,json_points,city[:city.index('.')]))
-                        continue
+                        res=gpd.sjoin(left_df=rectangles,
+                                    right_df=gdf,
+                                    how='right',
+                                    op='intersects')
 
-                    w= (gdf.iloc[0].geometry.bounds[2] - gdf.iloc[0].geometry.bounds[0])/split_cnt_x
-                    h= (gdf.iloc[0].geometry.bounds[3] - gdf.iloc[0].geometry.bounds[1])/split_cnt_y
-                    rect=make_mesh([gdf.iloc[0].geometry.bounds[0],gdf.iloc[0].geometry.bounds[1],gdf.iloc[0].geometry.bounds[2],gdf.iloc[0].geometry.bounds[3]],w,h)
-                    rectangles=gpd.GeoDataFrame(geometry=rect)
-                    rectangles['RectangleID']=range(len(rectangles))
+                        if len(new_points) > points_split:
+                            g=res.groupby('RectangleID')
+                            g=dict(list(g))
 
-                    res=gpd.sjoin(left_df=rectangles,
-                                right_df=gdf,
-                                how='right',
-                                op='intersects')
+                            ax=gdf.plot()
+                            color=['yellow','green','blue','black','purple','red']
+                            for k,v in g.items():
 
-                    if len(new_points) > points_split:
-                        g=res.groupby('RectangleID')
-                        g=dict(list(g))
+                                    row=k//split_cnt_x
+                                    col=k%split_cnt_y
+                                    
+                                    boundary = gpd.GeoSeries(rectangles['geometry'][k])
+                                    boundary.boundary.plot(ax=ax,color=color[(row%6+col)%6])
+                                    boundary_json = boundary.to_json()
+                                    boundary_dict = json.loads(boundary_json)
+                                    bbox = boundary_dict["features"][0]['bbox']
 
-                        ax=gdf.plot()
-                        color=['yellow','green','blue','black','purple','red']
-                        for k,v in g.items():
-
-                                row=k//split_cnt_x
-                                col=k%split_cnt_y
-                                
-                                boundary = gpd.GeoSeries(rectangles['geometry'][k])
-                                boundary.boundary.plot(ax=ax,color=color[(row%6+col)%6])
-                                boundary_json = boundary.to_json()
-                                boundary_dict = json.loads(boundary_json)
-                                bbox = boundary_dict["features"][0]['bbox']
-
-                                ix1=bbox[0] 
-                                iy1=bbox[1]
-                                ix2=bbox[2] 
-                                iy2=bbox[3]
-                                data_list.append((ix1,iy1,ix2,iy2,json_points,city))
-                                print("deal with inner bbox...")
+                                    ix1=bbox[0] 
+                                    iy1=bbox[1]
+                                    ix2=bbox[2] 
+                                    iy2=bbox[3]
+                                    data_list.append((ix1,iy1,ix2,iy2,json_points,city))
+                                    print("deal with inner bbox...")
         else:
             listDir(pathname)
 
@@ -155,8 +154,8 @@ def data_write_csv(file_name, datas):
 #y_split_size = float(sys.argv[3])
 #points_split = int(sys.argv[4])
 city_polygon_dir = './cities/'
-x_split_size = 1.0
-y_split_size = 0.5
-points_split = 3000
+x_split_size = 10
+y_split_size = 10
+points_split = 200000
 listDir(city_polygon_dir,x_split_size,y_split_size,points_split)
 data_write_csv('./polygon.csv',data_list)
