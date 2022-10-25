138b48bd226be514fcddb96ea0bc8de0618ad9e6
nieo2002 <115522184+nieo2002@users.noreply.github.com>
2022-10-25 15:41:25 +0800

    Update osm_test.py

--- src/osm_test.py
+++ src/osm_test.py
@@ -1,5 +1,5 @@
 #coding:utf-8
 from osmroads import osm_road_distances
 
-distances = osm_road_distances(province='Hefa')
+distances = osm_road_distances(level1='Gotland',country='Sweden')
 print(distances)
