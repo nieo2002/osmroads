b8d6066af870d2bacac142d6d320a355e86bdafb
nieo2002 <115522184+nieo2002@users.noreply.github.com>
2022-10-25 15:40:39 +0800

    Update osmroads.py

--- src/osmroads.py
+++ src/osmroads.py
@@ -1,17 +1,20 @@
 #coding:utf-8
 from cs.CentralSystem import fetch_data_from_ns
 
-def osm_road_distances(city=None,province=None,country=None,limit=100):
+def osm_road_distances(level3=None,level2=None,level1=None,country=None,limit=100):
     
-    sql_cmd = 'select city,province,x1,y1,x2,y2,polygon,country from city_boundary where '
-    if city is None and province is None and country is None:
+    sql_cmd = 'select level2,level1,x1,y1,x2,y2,polygon,country,level3 from city_boundary_standard where '
+    if level3 is None and level2 is None and level1 is None and country is None:
         return []
 
-    if city is not None:
-        sql_cmd += ' upper(city) = \'' + city.upper() + '\' and'
+    if level3 is not None:
+        sql_cmd += ' upper(level3) = \'' + level3.upper() + '\' and'
 
-    if province is not None:
-        sql_cmd += ' upper(province) = \'' + province.upper() + '\' and'
+    if level2 is not None:
+        sql_cmd += ' upper(level2) = \'' + level2.upper() + '\' and'
+
+    if level1 is not None:
+        sql_cmd += ' upper(level1) = \'' + level1.upper() + '\' and'
 
     if country is not None:
         sql_cmd += ' upper(country) = \'' + country.upper() + '\''
@@ -33,6 +36,7 @@ def osm_road_distances(city=None,province=None,country=None,limit=100):
             lat2 = data[5]
             npstr= data[6] 
             country = data[7]
+            county = data[8]
 
             #calculate city roads distances
             sql_cmd =  'select count(1) as roads,sum(distance) as distance ' + \
@@ -42,15 +46,16 @@ def osm_road_distances(city=None,province=None,country=None,limit=100):
                     'SELECT lsid,geo_distance(x1,y1,x2,y2) as distance  ' + \
                         'FROM ( ' + \
                         '  select lsid,osmid_start_x x1,osmid_start_y y1, osmid_end_x x2,osmid_end_y y2, pnpoly(osmid_start_x,osmid_start_y,\'' + npstr + '\') as pnpolys,pnpoly(osmid_end_x,osmid_end_y,\'' + npstr + '\') as pnpolye ' + \
-                        '  from osm_split_edge_roadnet_t  ' + \
+                        '  from osm_split_edge_roadnet_selected_t  ' + \
                         '  where osmid_start_x  < ' + str(lon2) + ' and osmid_start_x >  ' + str(lon1) + '  and osmid_start_y < ' + str(lat2) + '  and osmid_start_y > ' + str(lat1)  +  \
                         ' ) ' + \
                     'where  pnpolys = true and pnpolye = true' + \
                 ') ' + \
                 'group by lsid ' + \
             ')'
+            
             road = fetch_data_from_ns(sql_cmd)
-            result = {'country':country,'province':province,'city':city,'value':road}
+            result = {'country':country,'province':province,'city':city,'county':county,'value':road}
             roads.append(result)
             
     except Exception as error:
