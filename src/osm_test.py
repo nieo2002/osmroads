24991f2da49dd4ed2ffc7ee08c8350833e0488a9
nieo2002 <115522184+nieo2002@users.noreply.github.com>
2022-11-02 15:26:42 +0800

    Update osm_test.py

--- src/osm_test.py
+++ src/osm_test.py
@@ -1,5 +1,11 @@
 #coding:utf-8
 from osmroads import osm_road_distances
 
-distances = osm_road_distances(level1='Gotland',country='Sweden')
+distances = osm_road_distances(level3='余杭区',level2='杭州市',level1='浙江省',country='China')
+print(distances)
+
+distances = osm_road_distances(level2='济宁市',level1='山东省',country='China')
+print(distances)
+
+distances = osm_road_distances(level1='山东省',country='China')
 print(distances)
