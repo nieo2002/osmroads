f78e0c0956411ce9c5702d252707953bf90d8bcf
nieo2002 <115522184+nieo2002@users.noreply.github.com>
2022-11-02 15:26:13 +0800

    Update Polygon.py

--- src/polygon/Polygon.py
+++ src/polygon/Polygon.py
@@ -2,6 +2,8 @@
 import json
 import os
 import geopandas as gpd
+import shapely.wkt as wkt
+from shapely.geometry import MultiPolygon
 
 #根据文件名区分不同的polygon
 def build_polygon_from_files(dir):
@@ -45,7 +47,7 @@ def build_polygon_from_gadmfile(dir):
         pathname = os.path.join(dir, district)
         
         if (os.path.isfile(pathname)):
-            if pathname.endswith('.json') or pathname.endswith('.shp'):
+            if pathname.endswith('.json'):
 
                 gdf = gpd.read_file(pathname)
                 polygons = gdf.geometry.to_json()
@@ -92,6 +94,74 @@ def build_polygon_from_gadmfile(dir):
                     except Exception as error:
                         print(pathname)
                                 
+                    i = i + 1
+
+    return data_list
+
+def build_polygon_from_osm(dir):
+
+    data_list = []
+
+    roots = os.listdir(dir)
+    for district in roots:
+        pathname = os.path.join(dir, district)
+        
+        if (os.path.isfile(pathname)):
+            if pathname.endswith('.json') or pathname.endswith('.geojson'):
+
+                gdf = gpd.read_file(pathname)
+                polygons = gdf.geometry.to_json()
+                polygon_dict = json.loads(polygons)
+
+                for i in range(len(gdf)):
+
+                    try:
+                        district = gdf.iloc[i]
+
+                        #skip country boundary
+                        if district.parents is None:
+                            continue
+
+                        parents = district.parents.split(',')
+                        if len(parents) > 3:
+                            continue
+                        level = len(parents)
+                        
+                        X1=district.geometry.bounds[0] 
+                        Y1=district.geometry.bounds[1] 
+                        X2=district.geometry.bounds[2] 
+                        Y2=district.geometry.bounds[3] 
+
+                        #get distrinct attribute
+                        name= district.name
+                        name_en= district.name_en
+                        if name_en is None:
+                            name_en = ' '
+                        name_local = district.local_name
+                        if name_local is None:
+                            name_local = ' '
+                        
+                        name_local = name_local.replace(';',',').replace('"','')
+                        name = str(name).replace(';',',').replace('"','')
+                        name_en = name_en.replace(';',',').replace('"','')
+
+                        #parents = district.parents
+                        osmid = district.osm_id
+                        area = float(district.geometry.area)
+
+                        for parent in parents:
+
+                            #polygon_dict["features"][i]["geometry"]["coordinates"]
+                            for polygon in polygon_dict["features"][i]["geometry"]["coordinates"]:
+                                
+                                if district.geometry.geom_type =='Polygon':
+                                    data_list.append((name,name_en,name_local,osmid,level,parent,area,X1,Y1,X2,Y2,str(polygon)))
+                                else:
+                                    for polygon_points in polygon:
+                                        data_list.append((name,name_en,name_local,osmid,level,parent,area,X1,Y1,X2,Y2,str(polygon_points)))
+                    except Exception as error:
+                        print(error)
+
                     i = i + 1
 
     return data_list
