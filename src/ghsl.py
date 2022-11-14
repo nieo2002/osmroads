58f716bbb3b2f86d58799c9b29e4ac22a40437c1
nieo2002 <115522184+nieo2002@users.noreply.github.com>
2022-11-14 12:48:22 +0800

    Update ghsl.py

--- src/ghsl.py
+++ src/ghsl.py
@@ -1,7 +1,42 @@
 #coding:utf-8
 from cs.CentralSystem import fetch_data_from_ns
 
-def ghsl_population_query(country,level1,level2=None,level3=None,limit=1000):
+
+def ghsl_population_v2(cityname,limit=1000):
+    
+    sql_cmd = 'select distinct name.name, boundary.polygon, boundary.x1,boundary.y1,boundary.x2,boundary.y2 from osm_city_names name, osm_city_boundary boundary where name.osm_id = boundary.osmid and name.name=\'' +  cityname + '\' ' 
+    sql_cmd += ' limit ' + str(limit)
+    populations = []
+
+    try:
+        pologyons = fetch_data_from_ns(sql_cmd)
+        for data in pologyons['value']:
+            city = data[0]
+            lon1 = data[2]
+            lat1 = data[3]
+            lon2 = data[4]
+            lat2 = data[5]
+            npstr= data[1] 
+
+            #calculate city roads distances
+            sql_cmd =  ' SELECT sum(population) as population ' + \
+                        ' FROM ( ' + \
+                        '  select population,pnpoly(x,y,\'' + npstr + '\') as pnpoly ' + \
+                        '  from ghsl_population  ' + \
+                        '  where x  < ' + str(lon2) + ' and x >  ' + str(lon1) + '  and y < ' + str(lat2) + '  and y > ' + str(lat1)  +  \
+                        ' ) ' + \
+                        ' where  pnpoly = true '
+            
+            population = fetch_data_from_ns(sql_cmd)
+            result = {'city':cityname,'value':population}
+            populations.append(result)
+            
+    except Exception as error:
+        print(error)
+    
+    return populations
+
+def ghsl_population(country,level1,level2=None,level3=None,limit=1000):
     
     sql_cmd = ''    
 
