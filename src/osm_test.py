8271331e2b9dfbc412907235cfbeb0a3e9ea31bf
nieo2002 <115522184+nieo2002@users.noreply.github.com>
2022-11-14 12:41:12 +0800

    Update osm_test.py

--- src/osm_test.py
+++ src/osm_test.py
@@ -1,11 +1,8 @@
 #coding:utf-8
-from osmroads import osm_road_distances
+from osmroads import osm_road_lengths_v2
 
-distances = osm_road_distances(level3='余杭区',level2='杭州市',level1='浙江省',country='China')
-print(distances)
+distance = osm_road_lengths_v2('Weißpriach')
+print(distance)
 
-distances = osm_road_distances(level2='济宁市',level1='山东省',country='China')
-print(distances)
 
-distances = osm_road_distances(level1='山东省',country='China')
-print(distances)
+
