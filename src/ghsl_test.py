218e210d7261b00043c9405f4835d8f8e02a09a7
nieo2002 <115522184+nieo2002@users.noreply.github.com>
2022-11-25 11:53:52 +0800

    Update ghsl_test.py

--- src/ghsl_test.py
+++ src/ghsl_test.py
@@ -1,8 +1,6 @@
 #coding:utf-8
 from ghsl import ghsl_population_v2
 
-populations = ghsl_population_v2('Weißpriach')
+populations = ghslpopulation('Weißpriach')
 print(populations)
 
-populations = ghsl_population(country='China',level1='山东省',level2='济南市',limit=1000)
-print(populations)
